a1d3b5a74c57231e2f2277513bc03cce16288e82
Saransh1120 <sargarbhit19@gmail.com>
2026-09-05 00:41:59 +0530

    intgerornot

---
+++ integerornot.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+     float x ;
+     cout<<" enter the number";
+     cin>>x;
+
+     if(x ==int(x)) cout<<" yes integer";
+     else cout<<"no";
+
+}
