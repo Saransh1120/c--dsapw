9896628afc676bd2ab06c8a6fab1572f5548576b
Saransh1120 <sargarbhit19@gmail.com>
2026-09-06 00:17:52 +0530

    Rangeofint

---
+++ conditional/rectangle.cpp
@@ -0,0 +1,22 @@
+//Take length and breadth of a rectangle as
+ //input and find the area of the rectangle
+
+ # include<iostream>
+ using namespace std;
+ int main(){
+ int length;
+ cout<<"enter the length";
+
+ int breadth;
+ cout<<"enter the breadth";
+
+ cin>>breadth>>length;
+
+ int area = length*breadth;
+
+ cout<<area;
+
+
+
+
+ }
