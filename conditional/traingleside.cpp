009185c4d131d6110019d929d3ef89e12471cd63
Saransh1120 <sargarbhit19@gmail.com>
2026-09-06 13:56:17 +0530

    recenatagleside

--- conditional/traingleside.cpp
+++ conditional/traingleside.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     int x,y,z;
-    cout<<" enter the sides"
+    cout<<" enter the sides";
     cin>>x>>y>>z;
+// do side bari honi chiye 3rd side se bass
 
-
-    if(x==y && y==z){
+    if(x+y>z && y+z>x && x+z>y){
         cout<<"print ye sthe sides of traingle";
     }
     else cout<<" no ";
