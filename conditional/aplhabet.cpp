c9c9b793be6182af8d6771ded758aa65b121b9dc
Saransh1120 <sargarbhit19@gmail.com>
2026-09-06 14:17:26 +0530

    CAPSalhabet

---
+++ conditional/aplhabet.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+     char x;
+     cout<<" ente rthe alab" ;
+     cin>>x;
+     if (x>=65 && x<=98 ) cout<<"yes";
+     else cout <<"no";
+
+
+    return 0;
+}
