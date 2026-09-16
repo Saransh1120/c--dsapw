4450652ead7a27a80133e52e84c7312d2d4d59a6
Saransh1120 <sargarbhit19@gmail.com>
2026-09-16 12:09:25 +0530

    ruppesdollar

---
+++ basic/ruppestodollar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double rs;
+    cout<<"ente rthe ruppes ";
+    cin >> rs;
+
+    double dollar = rs * (1.0 / 100);
+    cout << dollar;
+}
