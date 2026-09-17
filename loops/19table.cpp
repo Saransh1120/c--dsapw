2084b6b117b799ad6da53397c6c67424793a5585
Saransh1120 <sargarbhit19@gmail.com>
2026-09-18 00:31:09 +0530

    print n to 1

---
+++ loops/19table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+    for(int i =1; i<=10; i++){
+        cout<<i*n<<" ";
+    }
+
+// for(int i = 19; i<=190;i+=19){
+//     cout<<i<<" ";
+// }
+
+
+}
---
+++ loops/oddprint.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+ int main () {
+
+//50 ilteration
+ for(int i = 2; i<=100; i+=2) {
+ cout<<i<<" ";
+    
+ }
+
+ }
