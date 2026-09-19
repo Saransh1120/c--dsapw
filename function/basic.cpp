a9fc9522380b4ca06da58acfaf9eb7cea2c3c215
Saransh1120 <sargarbhit19@gmail.com>
2026-09-20 01:23:45 +0530

    functions

---
+++ function/basic.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+//Q18: Take mass m and velocity v and calculate Kinetic Energy.
+//Formula: KE = ½ × m × v²
+using namespace std;
+
+
+void fun () {
+
+    cout<<"hi broo yoo        ";
+}
+
+int main (){ 
+    cout<<" hi daddy"<<endl;
+     
+    fun();
+
+    
+   }
+     
+
+
+
+
