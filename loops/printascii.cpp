6cc05a3c1a2d43dd13775c9d5e08368b4f466e4f
Saransh1120 <sargarbhit19@gmail.com>
2026-09-19 10:13:03 +0530

    factor

---
+++ loops/printascii.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int n;
+    cout<<"Enter the number ";
+    cin>>n;
+    
+   int a=65 , r=1;
+    for(int i=0;i<=n;i++) {
+        cout<<char(a)<<" ";
+       a+=r; }
+      
+}
+    
+
